5eb7e3fd4e97b34a44dbe9cc060486f6cfc418c3
foxyfool <rahulkumarmaity9@gmail.com>
2024-07-06 05:05:37 +0530

    rightCommit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,7 @@ import { substraction } from '@foundation/sample-lib'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   console.log('Substraction:', substraction(11, 5))
   await app.listen(3000)
 }
